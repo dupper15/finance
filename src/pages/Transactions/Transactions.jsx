31c6a633afc2ba9910276643245509528ac69f24
Lyon <fopop2@gmail.com>
2025-07-02 01:27:33 +0700

    fixed transaction

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -208,19 +208,19 @@ export function Transactions() {
                                     Thêm
                                 </button>
 
-                                <button
-                                    onClick={toggleBulkActions}
-                                    className={`inline-flex items-center px-4 py-2 text-sm font-medium border rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${
-                                        showBulkActions
-                                            ? 'text-blue-700 bg-blue-50 border-blue-300'
-                                            : 'text-gray-700 bg-white border-gray-300 hover:bg-gray-50'
-                                    }`}
-                                >
-                                    <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                    Xóa
-                                </button>
+                                {/*<button*/}
+                                {/*    onClick={toggleBulkActions}*/}
+                                {/*    className={`inline-flex items-center px-4 py-2 text-sm font-medium border rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${*/}
+                                {/*        showBulkActions*/}
+                                {/*            ? 'text-blue-700 bg-blue-50 border-blue-300'*/}
+                                {/*            : 'text-gray-700 bg-white border-gray-300 hover:bg-gray-50'*/}
+                                {/*    }`}*/}
+                                {/*>*/}
+                                {/*    <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">*/}
+                                {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />*/}
+                                {/*    </svg>*/}
+                                {/*    Xóa*/}
+                                {/*</button>*/}
 
                                 {/*<button*/}
                                 {/*    className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50"*/}
