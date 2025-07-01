8d6865317e177552e4ad01edbd2eebcb65004782
Lyon <fopop2@gmail.com>
2025-07-02 01:13:39 +0700

    fixed transaction

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -222,14 +222,14 @@ export function Transactions() {
                                     Xóa
                                 </button>
 
-                                <button
-                                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
-                                >
-                                    <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                                    </svg>
-                                    Thao tác hàng loạt
-                                </button>
+                                {/*<button*/}
+                                {/*    className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50"*/}
+                                {/*>*/}
+                                {/*    <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">*/}
+                                {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />*/}
+                                {/*    </svg>*/}
+                                {/*    Thao tác hàng loạt*/}
+                                {/*</button>*/}
 
                                 {showBulkActions && selectedTransactions.length > 0 && (
                                     <button
